3418d83b5f95d160f3ab0539d00e932c0a12ff4b
Dayananda Gowda I J <dayanandagowda2002@gmail.com>
2024-07-19 15:29:56 +0530

    Update App.java

--- src/main/java/dayaso/org/App.java
+++ src/main/java/dayaso/org/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! hello hi preran" );
+        System.out.println( "Hello World! hello hi preran hi" );
     }
 }
